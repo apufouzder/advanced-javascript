4aa345b8bd5ed8a3e2ba26d2aa93b1a2fb02b3fa
Apu Fouzder <apufouzdar@gmail.com>
2021-02-02 00:54:25 +0800

    null vs undefined interview question

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+// Undefined 1
+
+// let number;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(10, 20);
+// console.log(result);
+
+// 2
+// function add2(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result2 = add2(5, 7);
+// console.log(result2);
+
+// 3
+const girlFriend = {name: "Angel", phone: 0123654};
+console.log(girlFriend.bf);
+
+// 4
+let fun = undefined;
+console.log(fun);
+
+// null
+let ages = [10, 15, 20];
+console.log(ages[11]);
