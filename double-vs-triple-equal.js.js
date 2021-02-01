90f3071d85c79d6a210bf2273073f5bba0490b7d
Apu Fouzder <apufouzdar@gmail.com>
2021-02-02 01:11:40 +0800

    double vs triple equal common interview question

---
+++ double-vs-triple-equal.js.js
@@ -0,0 +1,9 @@
+// (false=0 = true, true=1 = true,)
+const first = false;
+const second = 0;// 
+
+if(first === second){
+    console.log('condition is true');
+} else{
+    console.log('condition is false');
+}
